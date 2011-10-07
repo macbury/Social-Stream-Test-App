f0a38fb1da1d5037893351025a8e62cdac404e81
macbury <macbury@gmail.com>
2011-10-07 12:41:01 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialStreamTestApp::Application.config.secret_token = 'f1239a52a686398f4547b58a3a435566f91da6f6395e9a6b8fe919aa70e01109d7e315e28deffcbba91ef7e852c05615fe118cecd6c4d14fdadcb73d7d956d10'
